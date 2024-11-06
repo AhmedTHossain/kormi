1dd652cab1de0fc9b062793045625d52ff98878f
ahmedthossain <ahmedthossain@outlook.com>
2024-11-06 10:41:50 +0600

    Add visual icon on valid phone number input
    - optimize LoginActivity code
    - add dependency for leakCanary library for memory leak detection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // leakCanary is a memory leak detection library for Android.
+    // debugImplementation because LeakCanary should only run in debug builds.
+    debugImplementation(libs.leakcanary.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/apptechbd/kormi/auth/presentation/login/LoginActivity.java
+++ app/src/main/java/com/apptechbd/kormi/auth/presentation/login/LoginActivity.java
@@ -1,13 +1,16 @@
 package com.apptechbd.kormi.auth.presentation.login;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.apptechbd.kormi.R;
@@ -30,11 +33,7 @@ public class LoginActivity extends BaseActivity {
         EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false); // Handles padding for system bars
 
         saveLocale("bn");
         setLocale(new Locale("bn"));
@@ -55,14 +54,20 @@ public class LoginActivity extends BaseActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 // Remove error message as soon as user starts typing
-                if (s.length() > 0) {
+                if (s.length() > 0)
                     binding.phoneInputLayout.setError(null);
-                }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-
+                if (s.length() == 0)return;
+                Drawable drawable;
+                if (PhoneNumberValidator.isValidBangladeshiMobileNumber(s.toString())) {
+                    drawable = ContextCompat.getDrawable(LoginActivity.this, R.drawable.ic_correct_input);
+                } else {
+                    drawable = ContextCompat.getDrawable(LoginActivity.this, R.drawable.ic_alert);
+                }
+                binding.phoneInputText.setCompoundDrawablesWithIntrinsicBounds(null, null, drawable, null);
             }
         });
 
@@ -75,9 +80,8 @@ public class LoginActivity extends BaseActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 // Remove error message as soon as user starts typing
-                if (s.length() > 0) {
+                if (s.length() > 0)
                     binding.pinInputLayout.setError(null);
-                }
             }
 
             @Override
@@ -90,22 +94,18 @@ public class LoginActivity extends BaseActivity {
     }
 
     private void validateFields() {
-        if (binding.phoneInputText.getText().toString().isEmpty()) {
+        String phoneNumber = binding.phoneInputText.getText().toString();
+        if (phoneNumber.isEmpty()) {
             binding.phoneInputLayout.setError(getString(R.string.error_empty_phone_field));
-        } else {
-            Log.d("PhoneNumberInput", "Phone number entered: " + binding.phoneInputText.getText().toString());
-            // Check if the phone number is valid only if it is not empty
-            if (!PhoneNumberValidator.isValidBangladeshiMobileNumber(binding.phoneInputText.getText().toString())) {
-                binding.phoneInputLayout.setError(getString(R.string.error_invalid_phone_number));
-            }
+        } else if (!PhoneNumberValidator.isValidBangladeshiMobileNumber(phoneNumber)) {
+            binding.phoneInputLayout.setError(getString(R.string.error_invalid_phone_number));
         }
-        if (binding.pinInputText.getText().toString().isEmpty()) {
+
+        String pin = binding.pinInputText.getText().toString();
+        if (pin.isEmpty()) {
             binding.pinInputLayout.setError(getString(R.string.error_empty_pin_field));
-        } else {
-            Log.d("PinInput", "Pin entered: " + binding.pinInputText.getText().toString());
-            // Check if the pin is valid only if it is not empty
-            if (binding.pinInputText.getText().length() != 5)
-                binding.pinInputLayout.setError(getString(R.string.error_invalid_pin_number));
+        } else if (pin.length() != 5) {
+            binding.pinInputLayout.setError(getString(R.string.error_invalid_pin_number));
         }
     }
 }
